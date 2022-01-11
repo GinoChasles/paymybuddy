cbf671c347f6c0b91af1784c1db3c66d79a74a43
Gino Chasles <gchasles@mgstream.com>
2022-01-11 17:20:50 +0100

    add constructor and getter/setter in user entity

--- src/main/java/com/gino/paymybuddy/model/User.java
+++ src/main/java/com/gino/paymybuddy/model/User.java
@@ -65,4 +65,105 @@ public class User {
   inverseJoinColumns = @JoinColumn(name = "idTransaction"))
   private List<Transaction> transactions;
 
+
+  public User() {
+  }
+
+  public User(final String usernameParam, final String passwordParam, final String emailParam,
+              final double accountBalanceParam) {
+    username = usernameParam;
+    password = passwordParam;
+    email = emailParam;
+    accountBalance = accountBalanceParam;
+  }
+
+  public int getIdUser() {
+    return idUser;
+  }
+
+  public void setIdUser(final int idUserParam) {
+    idUser = idUserParam;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(final String usernameParam) {
+    username = usernameParam;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(final String passwordParam) {
+    password = passwordParam;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(final String emailParam) {
+    email = emailParam;
+  }
+
+  public double getAccountBalance() {
+    return accountBalance;
+  }
+
+  public void setAccountBalance(final double accountBalanceParam) {
+    accountBalance = accountBalanceParam;
+  }
+
+  public BankAccount getBankAccount() {
+    return bankAccount;
+  }
+
+  public void setBankAccount(final BankAccount bankAccountParam) {
+    bankAccount = bankAccountParam;
+  }
+
+  public List<Transaction> getTransactionsEmit() {
+    return transactionsEmit;
+  }
+
+  public void setTransactionsEmit(
+      final List<Transaction> transactionsEmitParam) {
+    transactionsEmit = transactionsEmitParam;
+  }
+
+  public List<Transaction> getTransactionsReceiver() {
+    return transactionsReceiver;
+  }
+
+  public void setTransactionsReceiver(
+      final List<Transaction> transactionsReceiverParam) {
+    transactionsReceiver = transactionsReceiverParam;
+  }
+
+  public List<Commission> getCommissions() {
+    return commissions;
+  }
+
+  public void setCommissions(final List<Commission> commissionsParam) {
+    commissions = commissionsParam;
+  }
+
+  public List<User> getFriends() {
+    return friends;
+  }
+
+  public void setFriends(final List<User> friendsParam) {
+    friends = friendsParam;
+  }
+
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(final List<Transaction> transactionsParam) {
+    transactions = transactionsParam;
+  }
 }
