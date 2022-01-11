ad20dfcb7c2b43584dd5e368dc8eae96656e0325
Gino Chasles <gchasles@mgstream.com>
2022-01-11 15:31:06 +0100

    add structure restcontroller and requestmapping for all entities

---
+++ src/main/java/com/gino/paymybuddy/controller/CommissionController.java
@@ -0,0 +1,9 @@
+package com.gino.paymybuddy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("commission")
+public class CommissionController {
+}
